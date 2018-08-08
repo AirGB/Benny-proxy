24b02a99e19247445fdc9d1533b3cead4760d564
baklavacado <benny.l.wu@gmail.com>
2018-08-07 20:01:28 -0700

    add new proxy files

--- about-service/client/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-
-ReactDOM.render(<App id='86' listingId='56' />,document.getElementById('app'));
--- hero-photo-service/client/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Hero from './components/Hero.jsx';
-
-ReactDOM.render(<Hero />, document.getElementById('hero'));
